1c30a2e89195339b9807383ca8c63cf8c21e564e
marekcapliar <113228472+marekcapliar@users.noreply.github.com>
2023-02-26 10:19:33 +0100

    Add files via upload

--- vektory.py
+++ vektory.py
@@ -12,7 +12,7 @@ y_vector = b[1] - a[1]
 vector = [abs(x_vector), abs(y_vector)]
 small_vector = sorted(vector)[0]
 if small_vector != 0:
-    k = [x_vector//small_vector, y_vector//small_vector]
+    k = [int(x_vector/small_vector), int(y_vector/small_vector)]
 else:
     k = [x_vector, y_vector]
     small_vector = 1
@@ -21,15 +21,29 @@ img = Image.new('RGB', (250, 250), 'white')
 pixels = img.load()
 
 count = sum(vector)
+errorx = 0      # abs(x_vector/small_vector - k[0])
+errory = 0      # abs(y_vector/small_vector - k[1])
 
 while count > 0:
     for x in range(abs(k[0])):
         pixels[a[0], a[1]] = (0, 0, 0)
         a[0] += x_vector//abs(x_vector)
         count -= 1
+        errorx += abs(x_vector/small_vector - k[0])
+        if errorx >= 1:
+            pixels[a[0], a[1]] = (0, 0, 0)
+            a[0] += x_vector//abs(x_vector)
+            count -= 1
+            errorx -= 1
     for y in range(abs(k[1])):
         pixels[a[0], a[1]] = (0, 0, 0)
         a[1] += y_vector//abs(y_vector)
         count -= 1
+        errory += abs(y_vector/small_vector - k[1])
+        if errory >= 1:
+            pixels[a[0], a[1]] = (0, 0, 0)
+            a[1] += y_vector//abs(y_vector)
+            count -= 1
+            errory -= 1
 
 img.show()
